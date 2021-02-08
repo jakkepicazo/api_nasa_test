02676a4fd0968931d29b47821b216451f5cac113
Jakke Picado <jakkepicazo@gmail.com>
2021-02-08 12:25:26 -0600

    add_new_index,_and_photos.

--- mars_photos.rb
+++ mars_photos.rb
@@ -11,5 +11,15 @@ def request(url,token = nil)
     return JSON.parse(response.read_body)
 end
 
+def buid_web_page(photos_data)
+    File.open('mars_index.html', 'w') do |file|
+        photos = photos_data["photos"]
+        photos.each do |photo|
+            file.puts "<img src='#{photo["img_src"]}'>"
+            
+        end
+        
+    end
+end    
 nasa_array = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos","qVOQFRUPzsRgkBdXaM2pxd2b6iOCQBkR3togl1r5")
  buid_web_page(nasa_array)
